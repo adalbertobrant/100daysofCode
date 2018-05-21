f62731a02c5157cecc638ebf129dd5d9c248d13f
Adalberto Caldeira Brant Filho <adalbertobrant@gmail.com>
2018-05-21 19:40:18 -0300

    Update 013.c
    Termino do código do dia 03

--- day03/013.c
+++ day03/013.c
@@ -3,3 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define constante 32
+
+char Converter (char letra){
+    int aux=0;
+    aux = letra;
+    return (char)(aux+constante);
+}
+int main(void){
+   
+  char letra;
+   printf("Entre uma letra para retornar sua minuscula\n");
+   scanf("%c",&letra);
+   printf("A letra minuscula eh %c\n",Converter(letra));
+
+return 0;
+}
+
